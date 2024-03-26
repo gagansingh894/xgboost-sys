8c3a7f9d1975c403693328233723059c03f9aed3
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2024-03-26 21:51:38 +0000

    feat: xgboost 2.0.3 bindings

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+#![allow(clippy::upper_case_acronyms)]
+
+use std::env;
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-
-
 #[cfg(test)]
 mod tests {
     use super::*;
--- wrapper.h
+++ wrapper.h
@@ -1,2 +1,2 @@
-#include <xgboost/c_api.h>
-#include <rabit/c_api.h>
+#include "xgboost/include/xgboost/c_api.h"
+#include "xgboost/rabit/include/rabit/c_api.h"
